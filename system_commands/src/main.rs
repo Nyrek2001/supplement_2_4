2b2f16495d43f3400179e1481c1003d76982d0ca
NyrekWheeler <142206465+Nyrek2001@users.noreply.github.com>
2025-04-01 15:51:33 -0400

    Update main.rs

--- system_commands/src/main.rs
+++ system_commands/src/main.rs
@@ -1,3 +1,14 @@
+mod echo;
+mod mkdir;
+mod git_commands;
+
+use echo::echo;
+use mkdir::mkdir;
+use git_commands::git_commands;
+
 fn main() {
-    println!("Hello, world!");
+    // Example usage
+    println!("{}", echo("Hello, World!"));
+    mkdir(5, "test_dir");
+    git_commands();
 }
